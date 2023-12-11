b0abe27bbf5243f1ddbf226eb012ea1d8ef42459
Thais-DN <142531680+Thais-DN@users.noreply.github.com>
2023-12-11 17:04:31 -0300

    iniciando page p/ acessar empresa individual

--- empresa/views.py
+++ empresa/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Tecnologias, Empresa
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.messages import constants
 
@@ -45,11 +45,25 @@ def nova_empresa(request):
         return redirect('/home/nova_empresa/')
 
 def empresas(request):
+    technologias_filtrar = request.GET.get('tecnologias')
+    nome_filtrar = request.GET.get('nome')
     empresas = Empresa.objects.all()
-    return render(request, 'empresas.html', {'empresas': empresas})
+
+    if technologias_filtrar:
+        empresas = empresas.filter(tecnologias = technologias_filtrar)
+    
+    if nome_filtrar:
+        empresas = empresas.filter(nome__icontains = nome_filtrar)
+
+    tecnologias = Tecnologias.objects.all()
+    return render(request, 'empresas.html', {'empresas': empresas, 'tecnologias': tecnologias})
 
 def excluir_empresa(request, id):
     empresa = Empresa.objects.get(pk=id)
     empresa.delete()
     messages.add_message(request, constants.SUCCESS, 'Empresa excluída com sucesso')
     return redirect('/home/empresas')
+
+def empresa(request, id):
+    empresa_unica = get_object_or_404(Empresa, id=id)
+    return render(request, 'empresa_unica.html', {'empresa': empresa_unica})
