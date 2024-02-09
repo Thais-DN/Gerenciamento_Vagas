39cf533681634aace7fe86cb2b165ed674b72fd3
Thais-DN <142531680+Thais-DN@users.noreply.github.com>
2024-02-09 10:27:02 -0300

    add estilo extra

--- empresa/urls.py
+++ empresa/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name="home"),
+    path('', views.home, name="home"),
     path('nova_empresa/', views.nova_empresa, name="nova_empresa"),
     path('empresas/', views.empresas, name="empresas"),
     path('excluir_empresa/<int:id>', views.excluir_empresa, name="excluir_empresa"),
